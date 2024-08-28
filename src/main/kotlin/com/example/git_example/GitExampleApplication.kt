50d644f56bc40edb0441a4dc00baf9ef4f59b513
stdiodh <playlistdh@gmail.com>
2024-08-28 10:26:06 +0900

    git push example

---
+++ src/main/kotlin/com/example/git_example/GitExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.git_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GitExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/git_example/GitExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.git_example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
